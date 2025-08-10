a8c640ca06ac29695caaa58f322beddebc365565
michaelfuchaka <fuchakamichael06@gmail.com>
2025-08-10 21:30:56 +0300

    Fix create_spicy_food to use correct parameter name

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -10,29 +10,43 @@ spicy_foods = [
         "heat_level": 3,
     },
     {
-        "name": "Mapo Tofu",
+        "name": "Mapo Tofu",                                                                                                                                                                                                                                                                                                                                                                                                                     
         "cuisine": "Sichuan",
         "heat_level": 6,
     },
 ]
 
 def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
     pass
 
 def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"] > 5]
     pass
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
+
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+ for food in spicy_foods:
+    if food["cuisine"] == cuisine:
+          return food
     pass
 
 def print_spiciest_foods(spicy_foods):
+   for food in get_spiciest_foods(spicy_foods):
+    print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
     pass
 
 def get_average_heat_level(spicy_foods):
-    pass
+   total_heat = sum(food["heat_level"] for food in spicy_foods)
+   return total_heat // len(spicy_foods) 
+pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
